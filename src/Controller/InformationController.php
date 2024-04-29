8aea994133b3039c113c9fb070fefb87e97379f1
Fortierthe <fortierthe@cy-tech.fr>
2024-04-29 08:43:16 +0200

    Début mailer

--- src/Controller/InformationController.php
+++ src/Controller/InformationController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+
 
 class InformationController extends AbstractController
 {
@@ -14,5 +17,20 @@ class InformationController extends AbstractController
         return $this->render('information/index.html.twig', [
             'controller_name' => 'InformationController',
         ]);
+    }
+
+    #[Route('/email', name: 'send_email')]
+
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('flowerstore@laposte.net')
+            ->to('fortierthe@cy-tech.fr')
+            ->subject('Sujet de l\'email')
+            ->text('Contenu du message')
+            ->html('<p>Contenu du message en HTML</p>');
+
+        $mailer->send($email);
+        return $this->render('email/envoye.html.twig');
     }
 }
